894cda108e6a5fd9b102eb815475712fe20d7924
AchillesGit <dennisadiki@gmail.com>
2025-11-08 12:37:38 +0100

    add vite plugin checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), checker({ typescript: true })],
   server: {
     host: true,
     port: 5173,
-    allowedHosts: [
-      // deine ngrok-Subdomain eintragen:
-      "6750a6026feb.ngrok-free.app",
-    ],
   },
 });
