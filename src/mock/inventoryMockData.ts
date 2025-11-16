63aea0acd333cd04f27fc01b6de87b85f39723c7
AchillesGit <dennisadiki@gmail.com>
2025-11-16 21:04:58 +0100

    implement new item and item images

--- src/mock/inventoryMockData.ts
+++ src/mock/inventoryMockData.ts
@@ -25,6 +25,30 @@ export const mockInventoryItems: ItemInstance[] = [
     position: { row: 0, col: 0 },
     level: 1,
   },
+  {
+    instanceId: "mock-inv-004",
+    blueprintId: "cannonBasic",
+    direction: 0,
+    origin: "inventory",
+    position: { row: 2, col: 0 },
+    level: 1,
+  },
+  {
+    instanceId: "mock-inv-005",
+    blueprintId: "rapidGatling",
+    direction: 0,
+    origin: "inventory",
+    position: { row: 4, col: 0 },
+    level: 1,
+  },
+  {
+    instanceId: "mock-inv-006",
+    blueprintId: "heavyCannon",
+    direction: 0,
+    origin: "inventory",
+    position: { row: 6, col: 0 },
+    level: 1,
+  },
 ];
 
 export const mockShopItems: ItemInstance[] = [
