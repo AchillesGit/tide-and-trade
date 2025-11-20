e1dc82dbf39de2c6149b54f87eedab91dcc5629c
AchillesGit <dennisadiki@gmail.com>
2025-11-20 20:11:02 +0100

    implement shop item list with preview image and cost

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,4 +1,5 @@
 import { useGameStore } from "../store/gameStore";
+import { resolveItem } from "../util/itemHelper";
 
 import type { FC } from "react";
 
@@ -9,6 +10,18 @@ const Shop: FC = () => {
     generateShopItems(5);
   }
 
-  return <div> TODO </div>;
+  const resolvedShopItems = shopItems.map((i) => resolveItem(i));
+
+  return (
+    <div>
+      {resolvedShopItems.map((item) => (
+        <div key={item.blueprintId} className="border p-2">
+          <div>{item.blueprintId}</div>
+          <div>{item.baseValue}</div>
+          <img alt={item.blueprintId} className="h-8" src={item.image} />
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Shop;
