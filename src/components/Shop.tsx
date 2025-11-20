8b54d091853f422d4cef5f19215170e442617194
AchillesGit <dennisadiki@gmail.com>
2025-11-20 20:24:55 +0100

    implement shop item hovering: show item stats in info card

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,10 +1,11 @@
 import { useGameStore } from "../store/gameStore";
+import { formatGold } from "../util/formatHelper";
 import { resolveItem } from "../util/itemHelper";
 
 import type { FC } from "react";
 
 const Shop: FC = () => {
-  const { shopItems, generateShopItems } = useGameStore();
+  const { shopItems, generateShopItems, setHoveredItem } = useGameStore();
 
   if (shopItems.length === 0) {
     generateShopItems(5);
@@ -15,9 +16,20 @@ const Shop: FC = () => {
   return (
     <div>
       {resolvedShopItems.map((item) => (
-        <div key={item.blueprintId} className="border p-2">
+        <div
+          key={item.blueprintId}
+          className="border p-2"
+          onMouseEnter={() => {
+            setHoveredItem(item);
+          }}
+          onMouseLeave={() => {
+            setHoveredItem(null);
+          }}
+        >
           <div>{item.blueprintId}</div>
-          <div>{item.baseValue}</div>
+          <span className="text-base text-yellow-600">
+            {formatGold(item.baseValue)}
+          </span>
           <img alt={item.blueprintId} className="h-8" src={item.image} />
         </div>
       ))}
