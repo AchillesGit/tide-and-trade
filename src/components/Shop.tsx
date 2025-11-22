ae55036f1f519c24d1d2e17179b3b8c3160c3969
AchillesGit <dennisadiki@gmail.com>
2025-11-22 08:04:16 +0100

    implemented a first draft function to sell items and refactor shop logic into own hook

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,44 +1,13 @@
+import useShop from "../hooks/useShop";
 import { useGameStore } from "../store/gameStore";
 import { formatGold } from "../util/formatHelper";
-import { resolveItem } from "../util/itemHelper";
 
 import type { FC } from "react";
 
-import type { Item } from "../types/inventoryTypes";
-
 const Shop: FC = () => {
-  const {
-    shopItems,
-    generateShopItems,
-    setHoveredItem,
-    gold,
-    removeGold,
-    setGrabbedItem,
-    removeShopItem,
-  } = useGameStore();
+  const { setHoveredItem, gold } = useGameStore();
 
-  if (shopItems.length === 0) {
-    generateShopItems(5);
-  }
-
-  /** Deducts 100 gold and regenerates 5 shop items if the player has enough gold. */
-  const refreshShopItems = () => {
-    if (gold >= 100) {
-      removeGold(100);
-      generateShopItems(5);
-    }
-  };
-
-  /** Purchases the item: deducts its cost, grabs it, and removes it from the shop. */
-  const buyShopItem = (item: Item) => {
-    if (gold >= item.baseValue) {
-      setGrabbedItem({ ...item });
-      removeGold(item.baseValue);
-      removeShopItem(item.instanceId);
-    }
-  };
-
-  const resolvedShopItems = shopItems.map((i) => resolveItem(i));
+  const { buyShopItem, refreshShopItems, resolvedShopItems } = useShop();
 
   return (
     <div className="bg-gray-800 text-white rounded-md p-3 shadow-md border-2 border-transparent min-w-72">
@@ -47,6 +16,11 @@ const Shop: FC = () => {
         <h1 className="text-lg font-bold">Shop</h1>
         <span className="text-base text-yellow-400">{formatGold(gold)}</span>
       </div>
+
+      <p className="text-xs text-gray-300 mb-4">
+        You can <span className="text-yellow-400">sell items</span> by grabbing
+        one and pressing <span className="font-bold">S</span>.
+      </p>
 
       {/* shop items list */}
       <div className="flex flex-col gap-2 mb-3">
